183ba1c60f5395202a2e003808fca3b0c0efbd9b
vinuthna <jainavinuthna@gmail.com>
2023-12-22 20:52:14 +0530

    roboshop shell scripts

---
+++ mongodb.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started excuted at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+echo -e "$2..installation $R FAILED $N"
+else
+echo -e "$2..installation $G SUCESS $N"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo -e "$R Error:please run with root user $N"
+exit 1
+else
+echo -e "you are $G root user $N"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copied MongoDB Repo"
