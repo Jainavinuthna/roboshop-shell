eb9bb592629a5bf257517f078df3917f1ea3d4cf
vinuthna <jainavinuthna@gmail.com>
2023-12-23 20:34:41 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -55,7 +55,7 @@ VALIDATE $? "Downloading catalogue app"
 
 cd /app &>> $LOGFILE
 
-unzip /tmp/catalogue.zip &>> $LOGFILE
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "unzipping catalogue"
 
