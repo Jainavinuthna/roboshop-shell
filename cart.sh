b03d811dbaf89a622b8c4bf0ae378120b3745759
vinuthna <jainavinuthna@gmail.com>
2023-12-24 16:31:16 +0530

    roboshop

---
+++ cart.sh
@@ -0,0 +1,87 @@
+#!bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.nkvj.cloud
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started excuted at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+echo -e "$2..installation $R FAILED $N"
+else
+echo -e "$2..installation $G SUCESS $N"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo -e "$R Error:please run with root user $N"
+exit 1
+else
+echo -e "you are $G root user $N"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current Nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enable Nodejs:18"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Installing Nodejs:18" 
+
+id roboshop 
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>> $LOGFILE
+
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading cart app"
+
+cd /app &>> $LOGFILE
+
+unzip -o /tmp/cart.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping cart"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "install dependencies"
+
+#use absloutle path because cart.service exist there
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
+VALIDATE $? "copying cart service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "cart daemon reload"
+
+systemctl enable cart &>> $LOGFILE
+
+VALIDATE $? "Enable cart"
+
+systemctl start cart &>> $LOGFILE
+
+VALIDATE $? "starting cart"
+
