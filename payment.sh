67ca101a5bc162a329e6375120e82a0d5db14714
vinuthna <jainavinuthna@gmail.com>
2023-12-24 20:38:35 +0530

    roboshop

--- payment.sh
+++ payment.sh
@@ -58,7 +58,7 @@ pip3.6 install -r requirements.txt &>> $LOGFILE
 
 VALIDATE $? "Installing Dependencies"
 
-cp /home/centos/roboshop-shell/payment.service/etc/systemd/system/payment.service &>> $LOGFILE
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
 
 VALIDATE $? "copying payment services"
 
